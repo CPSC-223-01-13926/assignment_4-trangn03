2b546f6add50f4871112f72211a902bbc9805ed7
Trang <trangn0102@csu.fullerton.edu>
2022-10-10 19:08:11 -0700

    Update some changes

--- weather.py
+++ weather.py
@@ -67,7 +67,7 @@ def report_daily(data, date):
             t = data[key]['t']
             h = data[key]['h']
             r = data[key]['r']
-            display = display + f'{mdy: <22}' + f'{tm: <10}' + f'{t: >11}' + f'{h: >10}' + f'{r: >10}\n'
+            display = display + f'{mdy:22}{tm:10}{t:11}{h:10}{r:10.2f}\n'
     
     return display
 
@@ -76,17 +76,17 @@ def report_historical(data):
     display += '                          Minimum      Maximum   Minumum   Maximum     Total\n'
     display += 'Date                  Temperature  Temperature  Humidity  Humidity  Rainfall\n'
     display += '====================  ===========  ===========  ========  ========  ========\n'
-    h = ''
+    temp = None
     for key in data:
-        if h == key[0:8]: 
+        date = key[0:8]
+        if temp == date: 
             continue
-        else:
-            mdy = calendar.month_name[int(h[4:6])]+ ' '+ str(int(h[6:8]))+ ',' + str(int(h[0:4]))
-            min_temp = min_temperature(data = data,h = key[0:8])
-            max_temp = max_temperature(data = data,h = key[0:8])
-            min_hum = min_humidity(data = data,h = key[0:8])
-            max_hum = max_humidity(data = data,h = key[0:8])
-            rain = tot_rain(data = data,h = key[0:8])
-            total = f'{rain:10.2f}'
-            display += f'{mdy: <20}{min_temp: >13}{max_temp:>13}{min_hum:>10}{max_hum:>10}{total:>10}' + "\n"
+        mdy = calendar.month_name[int(date[4:6])]+ ' '+ str(int(date[6:8]))+ ', '+ str(int(date[0:4]))
+        min_temp = min_temperature(data, date)
+        max_temp = max_temperature(data, date)
+        min_hum = min_humidity(data, date)
+        max_hum = max_humidity(data, date)
+        rain = tot_rain(data, date)
+        display += f'{mdy:20}{min_temp:13}{max_temp:13}{min_hum:10}{max_hum:10}{rain:10.2f}\n'
+        temp = date
     return display
