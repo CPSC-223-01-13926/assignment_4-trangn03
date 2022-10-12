76a98e52da5840cac1a856f594bea7f96484ef25
trangn03 <trangn0102@csu.fullerton.edu>
2022-10-12 08:45:45 -0700

    Changes some structure of the code

--- weather.py
+++ weather.py
@@ -68,7 +68,6 @@ def report_daily(data, date):
             h = data[key]['h']
             r = data[key]['r']
             display = display + f'{mdy:22}{tm:10}{t:11}{h:10}{r:10.2f}\n'
-    
     return display
 
 def report_historical(data):
@@ -76,17 +75,17 @@ def report_historical(data):
     display += '                          Minimum      Maximum   Minumum   Maximum     Total\n'
     display += 'Date                  Temperature  Temperature  Humidity  Humidity  Rainfall\n'
     display += '====================  ===========  ===========  ========  ========  ========\n'
-    temp = None
+    d = ''
     for key in data:
-        date = key[0:8]
-        if temp == date: 
+        if d == key[0:8]:
             continue
-        mdy = calendar.month_name[int(date[4:6])]+ ' '+ str(int(date[6:8]))+ ', '+ str(int(date[0:4]))
-        min_temp = min_temperature(data, date)
-        max_temp = max_temperature(data, date)
-        min_hum = min_humidity(data, date)
-        max_hum = max_humidity(data, date)
-        rain = tot_rain(data, date)
-        display += f'{mdy:20}{min_temp:13}{max_temp:13}{min_hum:10}{max_hum:10}{rain:10.2f}\n'
-        temp = date
+        else: 
+            d = key[0:8]
+            mdy = calendar.month_name[int(d[4:6])]+ ' '+ str(int(d[6:8]))+ ', '+ str(int(d[0:4]))
+            min_temp = min_temperature(data = data, date = d)
+            max_temp = max_temperature(data = data, date = d)
+            min_hum = min_humidity(data = data, date = d)
+            max_hum = max_humidity(data = data, date = d)
+            rain = tot_rain(data = data, date = d)
+            display += f'{mdy:20}{min_temp:13}{max_temp:13}{min_hum:10}{max_hum:10}{rain:10.2f}\n'
     return display
